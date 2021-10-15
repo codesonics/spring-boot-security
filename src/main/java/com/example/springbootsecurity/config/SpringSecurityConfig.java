a945a6a792687e3784282cbea27538c358e8501a
codesonic <yyc77510072>
2021-10-16 03:38:28 +0900

    jwt 적용

--- src/main/java/com/example/springbootsecurity/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springbootsecurity/config/SpringSecurityConfig.java
@@ -1,6 +1,12 @@
 package com.example.springbootsecurity.config;
 
+import com.example.springbootsecurity.filter.StopwatchFilter;
+import com.example.springbootsecurity.filter.TesterAuthenticationFilter;
+import com.example.springbootsecurity.jwt.JwtAuthenticationFilter;
+import com.example.springbootsecurity.jwt.JwtAuthorizationFilter;
+import com.example.springbootsecurity.jwt.JwtProperties;
 import com.example.springbootsecurity.user.User;
+import com.example.springbootsecurity.user.UserRepository;
 import com.example.springbootsecurity.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -12,39 +18,50 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
 
+    private final UserService userService;
+    private final UserRepository userRepository;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // basic authentication
         http.httpBasic().disable(); // basic authentication filter 비활성화
         // csrf
-        http.csrf();
+        http.csrf().disable();
         // remember-me
-        http.rememberMe();
+        http.rememberMe().disable();
+        // stateless
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // jwt filter
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class
+        ).addFilterBefore(
+                new JwtAuthorizationFilter(userRepository),
+                BasicAuthenticationFilter.class
+        );
         // authorization
         http.authorizeRequests()
                 // /와 /home은 모두에게 허용
-                .antMatchers("/", "/home", "/signup", "/h2-console/**").permitAll()
-
+                .antMatchers("/", "/home", "/signup").permitAll()
                 // hello 페이지는 USER 롤을 가진 유저에게만 허용
                 .antMatchers("/note").hasRole("USER")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .csrf()
-                .ignoringAntMatchers("/h2-console/**").disable()
-                .httpBasic();
+                .anyRequest().authenticated();
         // login
         http.formLogin()
                 .loginPage("/login")
@@ -53,7 +70,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         // logout
         http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies(JwtProperties.COOKIE_NAME);
     }
 
     @Override
@@ -61,7 +80,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         // 정적 리소스 spring security 대상에서 제외
 //        web.ignoring().antMatchers("/images/**", "/css/**"); // 아래 코드와 같은 코드입니다.
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-        web.ignoring().antMatchers("/h2-console/**");
     }
 
     /**
